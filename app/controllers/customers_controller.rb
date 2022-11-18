d1c1156e89bc5d2dab934e64a1267e818abef7ee
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-11-18 06:37:03 +0000

    [Update]ログイン機能

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 
 class Public::CustomersController < ApplicationController
-  before_action :authenticate_user!, except: [:top, :about]
-  before_action :ensure_correct_user, only: [:edit, :update]
+  # before_action :authenticate_user!, except: [:top, :about]
+  # before_action :ensure_correct_user, only: [:edit, :update]
   
   def show
     @editcustomer = Customer.new
