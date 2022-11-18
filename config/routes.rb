5cfaeb35060e99f8ec1a6bf66849cf8f0c73c753
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-11-18 04:58:12 +0000

    [Add]ルートの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,31 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "homes#top"
   devise_for :admin
   devise_for :customers
+  
+  root to: "homes#top"
   get "home/about"=>"homes#about"
   
   #admin
   namespace :admin do
+    resources :customers
     resources :orders, only:[:index,:show,:update]
-    
+    resources :items
+    resources :genres
+    resources :orders
+    resources :orders_details
   end
   
   
   #public
   scope module: :public do
   
-    #orders
+    resources :customers
     resources :orders, only:[:index, :show, :update]
+    resources :items
+    resources :cart_items
+    resources :shipping_addresses
   
   end
   
