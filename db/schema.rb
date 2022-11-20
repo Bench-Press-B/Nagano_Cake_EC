0d9c53209889d3b88637095781fc766106825c14
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-11-20 08:56:19 +0000

    [Add]カート機能の実装注文機能の一部実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_20_032718) do
+ActiveRecord::Schema.define(version: 2022_11_20_070328) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -112,7 +112,7 @@ ActiveRecord::Schema.define(version: 2022_11_20_032718) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.integer "postage", default: 800, null: false
-    t.integer "total_price", null: false
+    t.integer "total_price"
     t.integer "payment_method", default: 0, null: false
     t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
