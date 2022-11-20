8e05e2e1f5f45e9342ab2c14964cd0f1945ca49c
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-11-20 03:41:11 +0000

    [add]public会員の実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,51 @@
 class Public::CustomersController < ApplicationController
+
   def show
-    @editcustomer = Customer.new
     @customer = Customer.find(params[:id])
   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render "edit"
+    end
+  end
   
+  def unsubscribe
+  end
+  
+  def withdraw
+    @customer = Customer.find(params[:id])
+    @customer.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name)
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :postal_code, :address, :phone_number, :is_deleted)
+  end
+
+  def ensure_correct_user
+   @customer = Customer.find(params[:id])
+   unless @customer == current_user
+     redirect_to customer_path(current_user)
+   end
+  end
+  
+  def reject_user
+    @customer = Customer.find_by(name: params[:customer][:first_name][:last_name])
+    if @customer 
+      if @customer.valid_password?(params[:customer][:encrypted_password]) && (@customer.is_deleted == false)
+        redirect_to new_customer_registration
+      end
+    end
   end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,7 +5,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
-    #@orders = current_customer.orders
+    @orders = current_customer.orders.all
   end
 
   def show
@@ -22,8 +22,12 @@ class Public::OrdersController < ApplicationController
   end
   
   def confirm
-    
   end
+  
+   private
 
+  def customer_params
+    params.require(:order_details).permit(:taxed_price, :quantity, :status)
+  end
 
 end
