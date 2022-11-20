4c9d4498b3bd5409b90de9d19965feb7afb33259
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-11-20 03:42:49 +0000

    [Add]注文機能の一部カートの一部実装

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+
+  has_many :orders
+  has_many :cart_items, dependent: :destroy
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_19_051342) do
+ActiveRecord::Schema.define(version: 2022_11_20_032718) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -111,7 +111,7 @@ ActiveRecord::Schema.define(version: 2022_11_19_051342) do
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "address", null: false
-    t.integer "postage", null: false
+    t.integer "postage", default: 800, null: false
     t.integer "total_price", null: false
     t.integer "payment_method", default: 0, null: false
     t.integer "status", default: 0, null: false
@@ -126,18 +126,6 @@ ActiveRecord::Schema.define(version: 2022_11_19_051342) do
     t.string "address", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
