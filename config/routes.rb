b5b432a10130ad378ee73de88cf6bbb13fbd021e
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-11-21 04:19:17 +0000

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  devise_for :admins, controllers: {
+  devise_for :admin, controllers: {
     sessions: 'admin/sessions',
     passwords: 'admin/passwords',
     registrations: 'admin/registrations'
@@ -19,9 +19,7 @@ Rails.application.routes.draw do
 
   #admin
   namespace :admin do
-    resources :customers, only:[:show,:edit,:update]
-    get "customers/unsubscribe" =>"customers#unsubscribe", as: "unsubscribe"
-    patch "customers/withdraw" => "customers#withdraw", as: "withdraw"
+    resources :customers, only:[:index,:show,:edit,:update]
     resources :orders, only:[:index,:show,:update]
     resources :items, only:[:index,:new,:create,:show,:edit,:update]
     resources :genres, only:[:index,:create,:edit,:update]
@@ -31,17 +29,13 @@ Rails.application.routes.draw do
 
   #public
   scope module: :public do
-    resources :customers, only:[:index,:show,:edit,:update]
-    resources :orders, only:[:new,:index,:show,:create] do
-      collection do
-          post 'confirm'
-          get 'thanx'
-        end
-      end
+    resources :customers, only:[:show,:edit,:update]
+    get "customers/:id/unsubscribe" => "customers#unsubscribe", as: "unsubscribe"
+    patch "customers/:id/withdraw" => "customers#withdraw", as: "withdraw"
+    resources :orders, only:[:new,:index,:show,:create]
     resources :items, only:[:index,:show]
     resources :cart_items, only:[:index,:update,:dastroy,:create]
     delete "cart_items/all_destroy" => "public/cart_items#all_destroy", as: "all_destroy_cart_items"
-
     resources :shipping_addresses, only:[:index,:edit,:create,:update,:destroy]
 
   end
