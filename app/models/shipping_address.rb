8c4044b658f7f281b177df11b8eb72cd518fa2ee
ren6662 <114849966+ren6662@users.noreply.github.com>
2022-11-23 03:47:45 +0000

    レイアウト調整

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,6 +1,10 @@
 class ShippingAddress < ApplicationRecord
 
   belongs_to :customer
+  
+  validates :postal_code, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
 
   def order_address
     "〒" + self.postal_code + "   " + self.address+ "   " + self.name
