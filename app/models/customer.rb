d699e53213b785c6a78debd5b3cd46d84d7f0785
iam-yuya <112651372+iam-yuya@users.noreply.github.com>
2022-11-25 07:03:09 +0000

    fix

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :kana_first_name, presence: true
@@ -12,7 +12,7 @@ class Customer < ApplicationRecord
   validates :phone_number, presence: true
   validates :address, presence: true
   validates :email, presence: true
-  
+
 
   has_many :orders
   has_many :cart_items, dependent: :destroy
@@ -20,10 +20,10 @@ class Customer < ApplicationRecord
 
   def full_name
     self.first_name + " " + self.last_name
-  end         
+  end
 
   def active_for_authentication?
     super && (is_deleted == false)
   end
-  
+
 end
