e13e1df54609e468a0aae2e99c8ac23d26281966
EC2 Default User <ec2-user@ip-172-31-47-205.ap-northeast-1.compute.internal>
2022-11-25 08:15:17 +0000

    色々修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,16 +3,16 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :kana_first_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-  validates :kana_last_name, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-  validates :postal_code, presence: true, numericality: true
+  validates :kana_first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :postal_code, presence: true
   validates :phone_number, presence: true
   validates :address, presence: true
   validates :email, presence: true
-
+  
 
   has_many :orders
   has_many :cart_items, dependent: :destroy
@@ -20,10 +20,10 @@ class Customer < ApplicationRecord
 
   def full_name
     self.first_name + " " + self.last_name
-  end
+  end         
 
   def active_for_authentication?
     super && (is_deleted == false)
   end
-
+  
 end
